2e10ed04eaa11e7b8a6766b0720890cd0c4d8dda
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-03-17 07:16:56 +0530

    Count Pairs whose sum is equal to X.java

---
+++ 17-03-2024/Count Pairs whose sum is equal to X.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class count_pairs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n1 = sc.nextInt();
+            Integer arr1[] = new Integer[n1];
+
+            for (int i = 0; i < n1; i++) arr1[i] = sc.nextInt();
+
+            int n2 = sc.nextInt();
+            Integer arr2[] = new Integer[n2];
+
+            for (int i = 0; i < n2; i++) arr2[i] = sc.nextInt();
+
+            LinkedList<Integer> head1 = new LinkedList<>(Arrays.asList(arr1));
+            LinkedList<Integer> head2 = new LinkedList<>(Arrays.asList(arr2));
+
+            int x = sc.nextInt();
+            Solution gfg = new Solution();
+            System.out.println(gfg.countPairs(head1, head2, x));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+// your task is to complete this function
+
+/*
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+
+    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,
+                          int x) {
+        //your code Start
+         HashSet<Integer> set = new HashSet<>();
+
+        // Traverse the second list and store its elements in a set
+        for (Integer value : head2) {
+            set.add(value);
+        }
+
+        int count = 0;
+
+        // Traverse the first list and for each element, check if its complement exists in the set
+        for (Integer value : head1) {
+            if (set.contains(x - value)) {
+                count++;
+            }
+        }
+
+        return count;
+      //your code End
+    }
+}
