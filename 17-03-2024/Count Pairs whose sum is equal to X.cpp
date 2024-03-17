c7dd342b37086de7b6d9c4714db324a00fec9e6a
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-03-17 07:15:21 +0530

    Count Pairs whose sum is equal to X.cpp

---
+++ 17-03-2024/Count Pairs whose sum is equal to X.cpp
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** headRef, struct Node** tailRef, int newData) {
+    struct Node* new_node = new Node(newData);
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+    }
+
+    else {
+        (*tailRef)->next = new_node;
+    }
+    *tailRef = new_node;
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+  public:
+    // your task is to complete this function
+    int countPairs(struct Node* head1, struct Node* head2, int x) {
+        // Your Code Start
+           std::unordered_set<int> set;
+
+        // Traverse the second list and store its elements in a set
+        struct Node* temp = head2;
+        while (temp != nullptr) {
+            set.insert(temp->data);
+            temp = temp->next;
+        }
+
+        int count = 0;
+
+        // Traverse the first list and for each element, check if its complement exists in the set
+        temp = head1;
+        while (temp != nullptr) {
+            if (set.find(x - temp->data) != set.end()) {
+                count++;
+            }
+            temp = temp->next;
+        }
+
+        return count;
+      //Your Code End
+    
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        struct Node* head1 = NULL;
+        struct Node* tail1 = NULL;
+        struct Node* tail2 = NULL;
+        struct Node* head2 = NULL;
+        int n1, n2, tmp, x;
+        cin >> n1;
+        while (n1--) {
+            cin >> tmp;
+            append(&head1, &tail1, tmp);
+        }
+        cin >> n2;
+        while (n2--) {
+            cin >> tmp;
+            append(&head2, &tail2, tmp);
+        }
+        cin >> x;
+        Solution obj;
+        cout << obj.countPairs(head1, head2, x) << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
