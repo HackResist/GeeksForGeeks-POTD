6edd331f4c9200905c37c91e10f8575964483f83
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-03-24 00:19:15 +0530

    Insert an Element at the Bottom of a Stack.cpp

---
+++ 24-03-2024/Insert an Element at the Bottom of a Stack.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    stack<int> insertAtBottom(stack<int> st,int x){
+      //Your Code Start
+        
+          if (st.empty()) {
+            st.push(x);
+            return st;
+        } else {
+            int temp = st.top();
+            st.pop();
+            st = insertAtBottom(st, x);
+            st.push(temp);
+            return st;
+        }
+      //Your Code End
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n;
+        cin>>x;
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            st.push(a);
+        }
+        Solution ob;
+        stack<int> tmp=ob.insertAtBottom(st,x);
+        vector<int> v;
+        while(tmp.size()){
+            v.push_back(tmp.top());tmp.pop();
+        }
+        reverse(v.begin(),v.end());
+        for(int i=0;i<v.size();i++){
+            cout<<v[i];
+            if(i!=v.size()-1){
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
