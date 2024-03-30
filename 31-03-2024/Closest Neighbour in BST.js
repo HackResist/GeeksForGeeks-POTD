6f634b981d11be7cc04712cee48a5c287b626fde
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-03-31 00:22:20 +0530

    Closest Neighbour in BST.js

---
+++ 31-03-2024/Closest Neighbour in BST.js
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildTree(str) {
+    // Corner Case
+    if (str.length === 0 || str[0] === "N") return null;
+
+    // Create the root of the tree
+    let root = new Node(parseInt(str[0]));
+
+    // Push the root to the queue
+    let queue = [];
+    let start = 0;
+    queue.push(root);
+
+    // Starting from the second element
+    let i = 1;
+    while (queue.length !== start && i < str.length) {
+
+        // Get and remove the front of the queue
+        let currNode = queue[start];
+        start++;
+
+        // Get the current node's value from the string
+        let currVal = str[i];
+
+        // If the left child is not null
+        if (currVal !== "N") {
+
+            // Create the left child for the current node
+            currNode.left = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= str.length) break;
+        currVal = str[i];
+
+        // If the right child is not null
+        if (currVal !== "N") {
+
+            // Create the right child for the current node
+            currNode.right = new Node(parseInt(currVal));
+
+            // Push it to the queue
+            queue.push(currNode.right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let s_tree = readLine().trim().split(" ");
+        let x = parseInt(readLine());
+        let root = buildTree(s_tree);
+        let obj = new Solution();
+        let res = obj.findMaxForN(root, x);
+        console.log(res);
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+/**
+ * @param {Node} root
+ * @param {number} N
+ * @return {number}
+ */
+
+class Solution {
+    findMaxForN(root, n) {
+        //Your Code Start
+         let result = -1; // Initialize the result with -1
+
+        // Traverse the BST until root becomes null
+        while (root) {
+            // If current node's data is less than or equal to n, update result and move to right subtree
+            if (root.data <= n) {
+                result = root.data; // Update result
+                root = root.right; // Move to right subtree
+            } else {
+                root = root.left; // Move to left subtree
+            }
+        }
+
+        return result;
+        // Your Code End
+    }
+}
