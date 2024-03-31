3c6aaa48c572ffe636b0a1321557a131ab43c76d
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-04-01 00:23:52 +0530

    Pairs violating the BST property.java

---
+++ 1-04-2024/Pairs violating the BST property.java
@@ -0,0 +1,152 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    public static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.charAt(0) == 'N') return null;
+
+        // Creating array of Strings from input
+        // String after spliting by space
+        String ip[] = str.split(" ");
+
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static Node inputTree(BufferedReader br) throws IOException {
+        return buildTree(br.readLine().trim());
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Node root = Node.inputTree(br);
+
+            Solution obj = new Solution();
+            int res = obj.pairsViolatingBST(n, root);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution {
+    public static int pairsViolatingBST(int n, Node root) {
+      //Your Code Start
+        int r=0;
+        Stack<Node> st = new Stack<>();
+        int pre[] = new int[n];
+        Node t = root;
+        int i=0;
+        while(t!=null || !st.isEmpty()){
+          while(t!=null){
+                st.push(t);
+                t=t.left;
+            }  
+            t = st.pop();
+            pre[i++]=t.data;
+             t=t.right;
+        }
+        for(i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(pre[i]>=pre[j]){
+                    r++;
+                }
+            }
+        }
+        return r;
+      //Your Code End
+    
+    }
+}
