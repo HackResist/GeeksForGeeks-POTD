d4dbbef1cd924de66adadbfea3a58c2721a75844
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-04-06 08:28:19 +0530

    Count ways to N'th Stair.py

---
+++ 06-04-2024/Count ways to N'th Stair.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+
+class Solution:
+
+    #Function to count number of ways to reach the nth stair
+    #when order does not matter.
+    def countWays(self, n):
+        #Your Code Start
+         return int(n/2+1)
+        #Your Code End
+      #  mod = 1000000007
+    
+
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        ob = Solution()
+        print(ob.countWays(n))
+
+# } Driver Code Ends
