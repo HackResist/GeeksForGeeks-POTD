f21598c38cf79ce8917ee37fba49974a4ece97dd
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-04-06 08:25:09 +0530

    Count ways to N'th Stair.java

---
+++ 06-04-2024/Count ways to N'th Stair.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            // taking total number of stairs
+            int n = sc.nextInt();
+
+            // creating an object of class DynamicProgramming
+            Solution obj = new Solution();
+
+            // calling method of class countWays()
+            // of class DynamicProgramming
+            System.out.println(obj.countWays(n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+  //Your Code Start
+    int countWays(int n) {
+        // your code here
+         return n/2+1;
+      //Your Code End
+    }
+}
