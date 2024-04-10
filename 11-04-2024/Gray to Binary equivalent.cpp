e8be55f40b4b58830b957be9dca4ea6e0ccf38ff
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-04-11 00:37:25 +0530

    Gray to Binary equivalent.cpp

---
+++ 11-04-2024/Gray to Binary equivalent.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    int grayToBinary(int n)
+    {
+        //Your Code Start
+        int ans=n;
+        while(n!=0)
+        {
+        ans^=n>>1;
+        n=n>>1;
+        }
+    return ans;
+        
+        // Your Code End
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin>>n;//initializing n
+        
+        Solution ob;
+        //calling function grayToBinary()
+       cout<< ob.grayToBinary(n)<<endl;
+    }
+}
+
+// } Driver Code Ends
