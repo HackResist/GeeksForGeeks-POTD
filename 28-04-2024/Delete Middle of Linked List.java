428064fa9546bad64842653f4f4ac681b82b3b03
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-04-28 16:18:34 +0530

    Delete Middle of Linked List.java

---
+++ 28-04-2024/Delete Middle of Linked List.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            head = g.deleteMid(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+
+// } Driver Code Ends
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+         // If head is null or head is the only node in the list, return null
+      //Your Code Start
+        if (head == null || head.next == null)
+            return null;
+
+        Node slow = head;
+        Node fast = head;
+        Node prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // At this point, slow is pointing to the middle node
+        // Delete the middle node by adjusting pointers
+        prev.next = slow.next;
+
+        return head;
+      //Your Code End
+    }
+}
