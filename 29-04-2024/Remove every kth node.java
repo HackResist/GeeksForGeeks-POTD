a02d49b983814f49c157d37d3095b24acc715f9c
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-04-29 22:40:44 +0530

    Remove every kth node.java

---
+++ 29-04-2024/Remove every kth node.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+	Node next;
+	int data;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+class Delete_Kth_Node
+{
+	Node head;
+	Node tail;
+	
+	void addToTheLast(Node node){
+		if(head == null){
+		head = node;
+		tail = node;
+		}
+		else
+		{
+		    tail.next = node;
+		    tail = tail.next ;
+		}
+	}	
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			Delete_Kth_Node list = new Delete_Kth_Node();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            list.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				list.addToTheLast(new Node(a));
+			}
+			int k = sc.nextInt();					  
+			//System.out.println(list.head.data+" "+ k);
+			list.head = new Solution().delete(list.head,k); 
+			Node temp = list.head;
+			while(temp!=null)
+			{
+				System.out.print(temp.data+ " ");
+			    temp = temp.next;
+			}
+			System.out.println();
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/* Link list Node
+class Node
+{
+	Node next;
+	int data;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+class Solution
+{
+    /*You are required to complete this method*/
+    Node delete(Node head, int k)
+    {
+	// Your code Start
+	 if (k == 1) { // If k is 1, every node needs to be deleted, so return null
+            return null;
+        }
+        
+        Node dummy = new Node(0); // Create a dummy node to handle edge case when head needs to be deleted
+        dummy.next = head;
+        Node prev = dummy;
+        Node current = head;
+        int count = 0;
+        
+        while (current != null) {
+            count++;
+            
+            if (count % k == 0) { // If count is a multiple of k, delete the current node
+                prev.next = current.next;
+            } else {
+                prev = current;
+            }
+            
+            current = current.next;
+        }
+        
+        return dummy.next;
+      //Your Code End
+    }
+}
