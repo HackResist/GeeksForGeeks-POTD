2bfbda69faab8898ca8f58310e9e37ca9aad3062
LOKESH CHAUHAN <aeqxtr@gmail.com>
2024-07-05 14:27:56 +0530

    Vertical Width of a Binary Tree.java

---
+++ July 2024/05-07-2024/Vertical Width of a Binary Tree.java
@@ -0,0 +1,147 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution obj = new Solution();
+            int ans = obj.verticalWidth(root);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the vertical width of a Binary Tree.
+    public int verticalWidth(Node root) {
+        //Your Code Start
+        if (root == null) return 0;
+
+        // Use a set to store all the horizontal distances
+        HashSet<Integer> hdS = new HashSet<>();
+
+        // Queue to perform level order traversal
+        Queue<Pair> q = new LinkedList<>();
+
+        // Start with root at horizontal distance 0
+        q.add(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            Pair f = q.poll();
+            Node node = f.node;
+            int hd = f.hd;
+
+            // Insert the current horizontal distance to the set
+            hdS.add(hd);
+
+            // Process left and right children
+            if (node.left != null) q.add(new Pair(node.left, hd - 1));
+            if (node.right != null) q.add(new Pair(node.right, hd + 1));
+        }
+
+        // The number of unique horizontal distances gives the vertical width
+        return hdS.size();
+    }
+
+    // Helper class to store node with its horizontal distance
+    class Pair {
+        Node node;
+        int hd;
+
+        Pair(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+        //Your Code End
+    }
+}
