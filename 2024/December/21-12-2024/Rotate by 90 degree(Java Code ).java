4ede1b1b74770182b43ab9192d69e2f8528705a4
Lokesh Chauhan <dev.lokesh.eu@gmail.com>
2024-12-21 21:18:07 +0530

    Rotate by 90 degree(Java Code ).java

---
+++ 2024/December/21-12-2024/Rotate by 90 degree(Java Code ).java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int matrix[][] = new int[n][n];
+            // String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) System.out.print(matrix[i][j] + " ");
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+//Your Code Starts
+class Solution {
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int mat[][]) {
+    int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            int top = 0, bottom = n - 1;
+            while (top < bottom) {
+                int temp = mat[top][j];
+                mat[top][j] = mat[bottom][j];
+                mat[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+    }
+}
+//Your Code Ends
