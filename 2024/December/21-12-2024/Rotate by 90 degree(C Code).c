07f920f975cbcebca72eee31d3dd8a48e0542ba1
Lokesh Chauhan <dev.lokesh.eu@gmail.com>
2024-12-21 21:13:29 +0530

    Rotate by 90 degree(C Code).c

---
+++ 2024/December/21-12-2024/Rotate by 90 degree(C Code).c
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for C
+
+#include <stdio.h>
+
+
+// } Driver Code Ends
+// User function Template for C
+
+// Function to rotate matrix anticlockwise by 90 degrees.
+
+//Your Code Start
+void rotateby90(int n, int mat[][n]) {
+    // code here
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = mat[i][j];
+            mat[i][j] = mat[j][i];
+            mat[j][i] = temp;
+        }
+    }
+    for (int j = 0; j < n; j++) {
+        for (int i = 0, k = n - 1; i < k; i++, k--) {
+            int temp = mat[i][j];
+            mat[i][j] = mat[k][j];
+            mat[k][j] = temp;
+        }
+    }
+}
+
+//Your Code Ends
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d", &tc);
+    while (tc--) {
+        int n;
+        scanf("%d", &n);
+        int matrix[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                scanf("%d", &matrix[i][j]);
+            }
+        }
+
+        rotateby90(n, matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d ", matrix[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("~%s", "\n");
+    }
+    return 0;
+}
+// } Driver Code Ends
