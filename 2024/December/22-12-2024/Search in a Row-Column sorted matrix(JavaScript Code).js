4caa15958ee94296d0fcd7404e190ac9ed8c9f89
Lokesh Chauhan <dev.lokesh.eu@gmail.com>
2024-12-22 01:26:24 +0530

    Search in a Row-Column sorted matrix(JavaScript Code).js

---
+++ 2024/December/22-12-2024/Search in a Row-Column sorted matrix(JavaScript Code).js
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+/* Function to print an array */
+function printArray(arr, size) {
+    let i;
+    let s = '';
+    for (i = 0; i < size; i++) {
+        if (arr[i] == -0) arr[i] = 0;
+        s += arr[i];
+        s += " ";
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let n = parseInt(readLine());
+        let m = parseInt(readLine());
+        let matrix = new Array(n);
+        for (let i = 0; i < n; i++) {
+            let a = new Array(m);
+            let input_ar1 = readLine().split(' ').map(x => parseInt(x));
+            for (let j = 0; j < m; j++) a[j] = input_ar1[j];
+            matrix[i] = a;
+        }
+        let X = parseInt(readLine());
+        let obj = new Solution();
+        let ans = obj.matSearch(matrix, X);
+        if (ans)
+            console.log("true");
+        else
+            console.log("false");
+        console.log("~")
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {number[][]} mat
+ * @param {number} x
+ * @returns {bool}
+ */
+ 
+ //Your Code Starts
+class Solution {
+    matSearch(mat, x) {
+        let n = mat.length; 
+        let m = mat[0].length;
+        let row = 0, col = m - 1;
+        while (row < n && col >= 0) {
+            if (mat[row][col] === x) {
+                return true;
+            } else if (mat[row][col] > x) {
+                col--; 
+            } else {
+                row++; 
+            }
+        }
+        return false; 
+    }
+}
+//Your Code Ends
