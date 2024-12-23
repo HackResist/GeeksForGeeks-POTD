2ce181895b06a58534a683c0738579ae7d684934
Lokesh Chauhan <dev.lokesh.eu@gmail.com>
2024-12-23 23:28:45 +0530

    Search in a row-wise sorted matrix(Python Code).py

---
+++ 2024/December/23-12-2024/Search in a row-wise sorted matrix(Python Code).py
@@ -0,0 +1,43 @@
+
+#User function Template for python3
+
+#Your Code Starts
+class Solution:
+    
+    #Function to search a given number in row-column sorted matrix.
+    def searchRowMatrix(self, mat, x): 
+    	# code here 
+    	 return any(x in row for row in mat)
+    	
+#Your Code Ends
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        r = int(data[index])
+        c = int(data[index + 1])
+        index += 2
+        matrix = []
+        for i in range(r):
+            row = list(map(int, data[index:index + c]))
+            matrix.append(row)
+            index += c
+        x = int(data[index])
+        index += 1
+        ob = Solution()
+        if ob.searchRowMatrix(matrix, x):
+            print("true")
+        else:
+            print("false")
+        print("~")
+# } Driver Code Ends
